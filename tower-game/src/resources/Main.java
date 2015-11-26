122de1e3e541d6ff0d51eaaea255dd077a69bd61
buckleyjamesc <buckleyjamesc@gmail.com>
2015-11-26 01:29:23 -0500

    Readability of map editor

--- tower-game/src/resources/Main.java
+++ tower-game/src/resources/Main.java
@@ -1,5 +1,5 @@
 package resources;
-import java.applet.Applet;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -10,15 +10,10 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Line2D;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -39,28 +34,25 @@ public class Main {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
         return frame;
-    }
+	}
+	@SuppressWarnings("serial")
 	public static void main(String[] args) throws IOException {
 		try {
 			level = ImageIO.read(new File("src/resources/" + fileName + ".png"));
-	        lighten = ImageIO.read(new File("src/resources/lighten.png"));
-		 } catch(Exception e) {
-	          e.printStackTrace();
-	     }	
-        Main m = new Main();
+			lighten = ImageIO.read(new File("src/resources/lighten.png"));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		JFrame frame = buildFrame();
 		JPanel pane = new JPanel() {
-            @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(level, 0, 0, 800, 600, this);
         		g.drawImage(lighten, 0, 0, 800, 600, this);
-        		
         		g.setColor(Color.BLACK);
         		for(Line2D current : collisions){
         			g.drawLine((int)current.getX1(), (int)current.getY1(), (int)current.getX2(), (int)current.getY2());
         		}
-        		
         		g.setColor(Color.RED);
         		if(drawing) {
         			g.drawLine(tempx, tempy, mx, my);
@@ -68,13 +60,11 @@ public class Main {
             }
         };
 		pane.addMouseMotionListener(new MouseMotionListener() {
-			@Override
 			public void mouseDragged(MouseEvent arg0) {
 				mx  = arg0.getX();
 				my  = arg0.getY();
 				frame.repaint();
 			}
-			@Override
 			public void mouseMoved(MouseEvent arg0) {
 				mx  = arg0.getX();
 				my  = arg0.getY();
@@ -82,7 +72,6 @@ public class Main {
 			}
 		});
 		pane.addMouseListener( new MouseListener() {
-			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				if(drawing){
 					collisions.add(new Line2D.Double(tempx, tempy, mx, my));
@@ -93,35 +82,13 @@ public class Main {
 				}
 				drawing  = !drawing;
 				frame.repaint();
-				
 			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
+			public void mouseEntered(MouseEvent arg0) {}
+			public void mouseExited(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {}
 		});
 		frame.addKeyListener( new KeyListener() {
-			@Override
 			public void keyPressed(KeyEvent arg0) {
 				if(arg0.getKeyCode() == KeyEvent.VK_DELETE){
 					deleteLine();
@@ -132,18 +99,8 @@ public class Main {
 				frame.repaint();
 				
 			}
-
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
+			public void keyReleased(KeyEvent arg0) {}
+			public void keyTyped(KeyEvent arg0) {}
 		});
 		frame.getContentPane().setPreferredSize(new Dimension(800, 600));
         frame.pack();
