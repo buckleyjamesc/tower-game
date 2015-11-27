e4419cc8707a177936a2f351adc630c493d58642
robertbuckley <robertbuckleybee2@gmail.com>
2015-11-26 21:02:14 -0500

    DONE WITH DA DRAWINGS

--- tower-game/src/tower/GamePanel.java
+++ tower-game/src/tower/GamePanel.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.FileNotFoundException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,9 +21,19 @@ public class GamePanel extends JPanel implements Runnable, KeyListener, MouseLis
 	
 	public GamePanel(JFrame frame) {
 		this.frame = frame;
-		p = new Player(0,0);
+		p = new Player(1200,900);
 		// Does not work yet, not enough rooms
 		// rooms = WorldBuilder.buildWorld(width, height);
+		rooms = new Room[4][3];
+		for(int i = 0; i < rooms.length; i++){
+			for(int j = 0; j < rooms[i].length; j++){
+				rooms[i][j] = new Room("lighten");
+			}
+		}
+		rooms[1][1] = new Room("level");
+		rooms[2][1] = new Room("level2");
+		
+		
 	}
 	
 	private void update() {
@@ -31,9 +42,14 @@ public class GamePanel extends JPanel implements Runnable, KeyListener, MouseLis
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-		// TODO Auto-generated method stub
-        super.paintComponent(g);
-    }
+		super.paintComponent(g);
+		for(int i = 0; i < rooms.length; i++){
+			for(int j = 0; j < rooms[i].length; j++){
+				g.drawImage(rooms[i][j].getImage(), (int)(400 + 800*i - p.x), (int)(300 + 600*j - p.y), 800, 600,this);
+			}
+		}
+		g.drawRect(380, 280, 40, 40);
+	}
 
 	@Override
 	public void run() {
@@ -93,7 +109,18 @@ public class GamePanel extends JPanel implements Runnable, KeyListener, MouseLis
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			p.y -= 5;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			p.x -= 5;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			p.y += 5;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			p.x += 5;
+		}
 		
 	}
 
--- tower-game/src/tower/Room.java
+++ tower-game/src/tower/Room.java
@@ -1,11 +1,27 @@
 package tower;
 
+import java.awt.Image;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Room {
 	String filename;
-	Room(String filename){
+	public Room(String filename){
 		this.filename = filename;
 	}
-	
+	public Image getImage(){
+		
+		try {
+			return R.getImage(filename);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	
 }
