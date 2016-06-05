524eef04ffa7305b1bfbdd5cbe382735c5ef716b
buckleyjamesc <buckleyjamesc@gmail.com>
2016-06-05 00:57:07 -0400

    making the collisions more automated

--- tower-game/src/resources/MapEditor.java
+++ tower-game/src/resources/MapEditor.java
@@ -20,6 +20,8 @@ import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 public class MapEditor {
+	static final int DOOR_W = 60;
+	static final int DOOR_H = 100;
 	static String fileName = "level2";
 	static int mx = 400;
 	static int my = 300;
@@ -28,7 +30,12 @@ public class MapEditor {
 	static boolean drawing = false;
 	static Image level;
 	static Image lighten;
+	static boolean leftOpen = true;
+	static boolean rightOpen = true;
+	static boolean upOpen = true;
+	static boolean downOpen = true;
 	static ArrayList<Line2D> collisions = new ArrayList<Line2D>();
+	static ArrayList<Line2D> walls = new ArrayList<Line2D>();
 	private static JFrame buildFrame() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -43,6 +50,7 @@ public class MapEditor {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		remakeWalls();
 		JFrame frame = buildFrame();
 		JPanel pane = new JPanel() {
             protected void paintComponent(Graphics g) {
@@ -50,13 +58,25 @@ public class MapEditor {
                 g.drawImage(level, 0, 0, 800, 600, this);
         		g.drawImage(lighten, 0, 0, 800, 600, this);
         		g.setColor(Color.BLACK);
+        		for(Line2D current : walls){
+        			g.drawLine((int)current.getX1(), (int)current.getY1(), (int)current.getX2(), (int)current.getY2());
+        		}
         		for(Line2D current : collisions){
         			g.drawLine((int)current.getX1(), (int)current.getY1(), (int)current.getX2(), (int)current.getY2());
         		}
         		g.setColor(Color.RED);
+        			g.fillRect(0, 570-DOOR_H, 5, DOOR_H);
+        			g.fillRect(795, 570-DOOR_H, 5, DOOR_H);
+        			g.fillRect(400-DOOR_W/2, 0, DOOR_W, 5);
+        			g.fillRect(400-DOOR_W/2, 595, DOOR_W, 5);
         		if(drawing) {
         			g.drawLine(tempx, tempy, mx, my);
         		}
+        		g.setColor(Color.GREEN);
+    			if(leftOpen) g.fillRect(0, 570-DOOR_H, 5, DOOR_H);
+    			if(rightOpen) g.fillRect(795, 570-DOOR_H, 5, DOOR_H);
+    			if(upOpen) g.fillRect(400-DOOR_W/2, 0, DOOR_W, 5);
+    			if(downOpen) g.fillRect(400-DOOR_W/2, 595, DOOR_W, 5);
             }
         };
 		pane.addMouseMotionListener(new MouseMotionListener() {
@@ -95,9 +115,19 @@ public class MapEditor {
 					
 				} else if(arg0.getKeyCode() == KeyEvent.VK_ENTER){
 					writeFile();
+				} else if(arg0.getKeyCode() == KeyEvent.VK_LEFT){
+					leftOpen = !leftOpen;
+				} else if(arg0.getKeyCode() == KeyEvent.VK_RIGHT){
+					rightOpen = !rightOpen;
+				} else if(arg0.getKeyCode() == KeyEvent.VK_UP){
+					upOpen = !upOpen;
+				} else if(arg0.getKeyCode() == KeyEvent.VK_DOWN){
+					downOpen = !downOpen;
 				}
-				frame.repaint();
 				
+				remakeWalls();
+				
+				frame.repaint();
 			}
 			public void keyReleased(KeyEvent arg0) {}
 			public void keyTyped(KeyEvent arg0) {}
@@ -106,8 +136,38 @@ public class MapEditor {
         frame.pack();
         frame.add(pane);
 	}
+	public static void remakeWalls() {
+		walls = new ArrayList<Line2D>();
+		if(downOpen) {
+			walls.add(new Line2D.Double(leftOpen?0:30,570,400-DOOR_W/2,570));
+			walls.add(new Line2D.Double(400-DOOR_W/2,570,400-DOOR_W/2,600));
+			walls.add(new Line2D.Double(400+DOOR_W/2,600,400+DOOR_W/2,570));
+			walls.add(new Line2D.Double(400+DOOR_W/2,570,rightOpen?800:770,570));
+		} else {
+			walls.add(new Line2D.Double(leftOpen?0:30,570,rightOpen?800:770,570));
+		}
+		if(rightOpen) {
+			walls.add(new Line2D.Double(800,570-DOOR_H,770,570-DOOR_H));
+		}
+		walls.add(new Line2D.Double(770,rightOpen?570-DOOR_H:570,770,30));
+		if(upOpen) {
+			walls.add(new Line2D.Double(770,30,400+DOOR_W/2,30));
+			walls.add(new Line2D.Double(400+DOOR_W/2,30,400+DOOR_W/2,0));
+			walls.add(new Line2D.Double(400-DOOR_W/2,0,400-DOOR_W/2,30));
+			walls.add(new Line2D.Double(400-DOOR_W/2,30,30,30));
+		} else {
+			walls.add(new Line2D.Double(770,30,30,30));
+		}
+		walls.add(new Line2D.Double(30,30,30,leftOpen?570-DOOR_H:570));
+		if(leftOpen) {
+			walls.add(new Line2D.Double(30,570-DOOR_H,0,570-DOOR_H));
+		}
+	}
 	public static void writeFile() {
 		String fileString = "";
+		for(Line2D current : walls) {
+			//fileString += "" + current.getX1() + "," + current.getY1() + "," + current.getX2() + "," + current.getY2() + "\n";
+		}
 		for(Line2D current : collisions) {
 			fileString += "" + current.getX1() + "," + current.getY1() + "," + current.getX2() + "," + current.getY2() + "\n";
 		}
--- tower-game/src/tower/R.java
+++ tower-game/src/tower/R.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
+import java.awt.geom.Line2D.Double;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
@@ -23,11 +24,18 @@ public class R {
 	public static final Integer DOWN_KEY = KeyEvent.VK_S;
 	public static final Integer LEFT_KEY = KeyEvent.VK_A;
 	public static final Integer RIGHT_KEY = KeyEvent.VK_D;
+	public static final int DOOR_W = 60;
+	public static final int DOOR_H = 100;
 	private static Map<String, Image> imageMap = new HashMap<String, Image>();
 	private static Map<String, ArrayList<Line2D.Double>> collisionMap = new HashMap<String, ArrayList<Line2D.Double>>();
+	private static Map<Integer, ArrayList<Line2D.Double>> wallsMap = new HashMap<Integer, ArrayList<Line2D.Double>>();
 	private static Map<Integer, ArrayList<String>> data = new HashMap<Integer, ArrayList<String>>();
 	public static ArrayList<Image> walking = new ArrayList<Image>();
-	public static List<Image> jumping = new ArrayList<Image>();;
+	public static List<Image> jumping = new ArrayList<Image>();
+	public static Set<String> leftRooms = new HashSet<String>();
+	public static Set<String> rightRooms = new HashSet<String>();
+	public static Set<String> upRooms = new HashSet<String>();
+	public static Set<String> downRooms = new HashSet<String>();
 	public static BufferedImage gun;
 	public static BufferedImage bullet;
 	public static BufferedImage fists;
@@ -69,9 +77,10 @@ public class R {
 		return imageMap.get(fileName);
 	}
 	public static ArrayList<Line2D.Double> getCollisions(String fileName) throws FileNotFoundException {
+		String oldFileName = fileName;
 		fileName = "src/resources/" + fileName + ".collisions";
 		if(!collisionMap.containsKey(fileName)) {
-			ArrayList<Line2D.Double> collisions = new ArrayList<Line2D.Double>();
+			ArrayList<Line2D.Double> collisions = makeWalls(oldFileName);
 			Scanner scan = new Scanner(new File(fileName));
 			while(scan.hasNextLine()) {
 				String line = scan.nextLine();
@@ -90,6 +99,43 @@ public class R {
 		}
 		return collisionMap.get(fileName);
 	}
+	private static ArrayList<Double> makeWalls(String fileName) {
+		boolean leftOpen = leftRooms.contains(fileName);
+		boolean rightOpen = rightRooms.contains(fileName);
+		boolean upOpen = upRooms.contains(fileName);
+		boolean downOpen = downRooms.contains(fileName);
+		int code = (leftOpen?8:0) + (rightOpen?4:0) + (upOpen?2:0) + (downOpen?1:0);
+		if(!wallsMap.containsKey(code)) {
+			ArrayList<Line2D.Double> walls = new ArrayList<Line2D.Double>();
+			if(downOpen) {
+				walls.add(new Line2D.Double(leftOpen?0:30,570,400-DOOR_W/2,570));
+				walls.add(new Line2D.Double(400-DOOR_W/2,570,400-DOOR_W/2,600));
+				walls.add(new Line2D.Double(400+DOOR_W/2,600,400+DOOR_W/2,570));
+				walls.add(new Line2D.Double(400+DOOR_W/2,570,rightOpen?800:770,570));
+			} else {
+				walls.add(new Line2D.Double(leftOpen?0:30,570,rightOpen?800:770,570));
+			}
+			if(rightOpen) {
+				walls.add(new Line2D.Double(800,570-DOOR_H,770,570-DOOR_H));
+			}
+			walls.add(new Line2D.Double(770,rightOpen?570-DOOR_H:570,770,30));
+			if(upOpen) {
+				walls.add(new Line2D.Double(770,30,400+DOOR_W/2,30));
+				walls.add(new Line2D.Double(400+DOOR_W/2,30,400+DOOR_W/2,0));
+				walls.add(new Line2D.Double(400-DOOR_W/2,0,400-DOOR_W/2,30));
+				walls.add(new Line2D.Double(400-DOOR_W/2,30,30,30));
+			} else {
+				walls.add(new Line2D.Double(770,30,30,30));
+			}
+			walls.add(new Line2D.Double(30,30,30,leftOpen?570-DOOR_H:570));
+			if(leftOpen) {
+				walls.add(new Line2D.Double(30,570-DOOR_H,0,570-DOOR_H));
+			}
+			wallsMap.put(code, walls);
+		}
+		return wallsMap.get(code);
+	}
+
 	public static ArrayList<String> getPossibleRooms(boolean left, boolean right, boolean up, boolean down) throws FileNotFoundException {
 		int code = (left?8:0) + (right?4:0) + (up?2:0) + (down?1:0);
 		if(data.isEmpty()) {
@@ -99,10 +145,18 @@ public class R {
 				Scanner scan2 = new Scanner(line);
 				scan2.useDelimiter(",");
 				String fileName = scan2.next();
-				int readCode = 8*scan2.nextInt() + 4*scan2.nextInt() + 2*scan2.nextInt() + scan2.nextInt();
+				int roomLeft = scan2.nextInt();
+				int roomRight = scan2.nextInt();
+				int roomUp = scan2.nextInt();
+				int roomDown = scan2.nextInt();
+				int readCode = 8*roomLeft + 4*roomRight + 2*roomUp + roomDown;
 				if(!data.containsKey(readCode)) data.put(readCode, new ArrayList<String>());
 				System.out.println("Scanning line '" + line + "' of file " + "src/resources/data.world" + ", " + readCode + " -> \"" + fileName + "\"");
 				data.get(readCode).add(fileName);
+				if(0!=roomLeft) leftRooms.add(fileName);
+				if(0!=roomRight) rightRooms.add(fileName);
+				if(0!=roomUp) upRooms.add(fileName);
+				if(0!=roomDown) downRooms.add(fileName);
 				scan2.close();
 			}
 			scan.close();
