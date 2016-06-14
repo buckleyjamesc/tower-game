b31e1a51f53239ff24ab25378e72b878d41a51df
buckleyjamesc <buckleyjamesc@gmail.com>
2016-06-13 21:14:44 -0400

    Made hud change location based on screen size

--- tower-game/src/tower/Hud.java
+++ tower-game/src/tower/Hud.java
@@ -12,7 +12,7 @@ public class Hud implements Drawable{
 	double stamRegin;
 	final double STARTINGSTAM = 100;
 	final double STARTINGHP = 400;
-	final int HEIGHT = 75;
+	final int HEIGHT = 50;
 	final int LENGTH = 400;
 
 	public Hud(){
@@ -27,13 +27,13 @@ public class Hud implements Drawable{
 	public void draw(Graphics g) {
 		//assumes 1920x1080, draws bars for stamina and hp
 		g.setColor(Color.BLACK);
-		g.drawRect(5, 1080 - HEIGHT - 25, (int) LENGTH, HEIGHT);
+		g.drawRect(5, (int) (R.getHeight()) - HEIGHT - 25, (int) LENGTH, HEIGHT);
 		g.setColor(Color.red);
-		g.fillRect(6, 1080 - HEIGHT - 25 + 1, (int) (hp / maxHP * LENGTH) - 1, HEIGHT - 1);
+		g.fillRect(6, (int) (R.getHeight()) - HEIGHT - 25 + 1, (int) (hp / maxHP * LENGTH) - 1, HEIGHT - 1);
 		g.setColor(Color.BLACK);
-		g.drawRect(10 + LENGTH, 1080 - HEIGHT - 25, (int) LENGTH, HEIGHT);
+		g.drawRect(10 + LENGTH, (int) (R.getHeight()) - HEIGHT - 25, (int) LENGTH, HEIGHT);
 		g.setColor(Color.YELLOW);
-		g.fillRect(11 + LENGTH, 1080 - HEIGHT - 25 + 1, (int) (stamina / maxStam * LENGTH) - 1, HEIGHT - 1);
+		g.fillRect(11 + LENGTH, (int) (R.getHeight()) - HEIGHT - 25 + 1, (int) (stamina / maxStam * LENGTH) - 1, HEIGHT - 1);
 
 	}
 	public void update(){
